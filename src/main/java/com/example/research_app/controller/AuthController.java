293209ba6e50e95d37955f81ae8cd48291cdd338
GhofraneZnina <ghofranezenina@gmail.com>
2025-04-11 12:11:05 +0100

    commit

--- src/main/java/com/example/research_app/controller/AuthController.java
+++ src/main/java/com/example/research_app/controller/AuthController.java
@@ -35,7 +35,7 @@ public ResponseEntity<?> login(@RequestBody AuthRequest request) {
     }
 }
 }
-//authrequest
+//authrequestchange
 class AuthRequest {
     private String email;
     private String password;
